937122cc595077ee1385c7b6a38c0cc8786f9812
Isya Haishi <isyahaishi@gmail.com>
2026-04-08 17:59:40 +0700

    x
    xz

--- telegram.py
+++ telegram.py
@@ -30,6 +30,10 @@ async def main(channels):
     for ch in channels:
         messages = await client.get_messages(ch, limit=1)
         for msg in messages:
-            print(f"[{ch}] {msg.text}")
+            if msg.text:
+                print(f"[{ch}] {msg.text}")
+            if msg.photo:
+                    file_path = await msg.download_media(file=f"downloads/{ch}_")
+                    print(f"[{ch}] Photo saved: {file_path}")
 with client:
     client.loop.run_until_complete(main(channels))
